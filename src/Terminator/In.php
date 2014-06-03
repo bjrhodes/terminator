d03080b53c55b5426a4ed62516d0a0cad52de433
Barry Rhodes <barry.rhodes@macace.net>
2014-06-03 15:03:41 +0100

    added handy yesno question feture

--- src/Terminator/In.php
+++ src/Terminator/In.php
@@ -3,7 +3,25 @@ namespace Terminator;
 
 class In
 {
-    // @todo extend for users with no readline support
+    // @todo extend for users with no native readline support
+    public function yesNo($question, $default = "Y")
+    {
+        if ($default === "Y") {
+            $msg = $question . " [Y/n]";
+        } else {
+            $msg = $question . " [y/N]";
+        }
+
+        $response = $this->readline($msg);
+        if (trim($response) === '') {
+            $char = $default;
+        } else {
+            $char = strtoupper(substr($response, 0, 1));
+        }
+
+        return ($char === "Y");
+    }
+
     public function readLine($prompt = null)
     {
         return readline($prompt);
